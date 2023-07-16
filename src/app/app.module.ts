a0366d1010b316677e55f49375e416b0ce7f7f53
arek <arek@server.com>
2023-07-16 15:02:46 +0200

    Empty components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './feature/form/form.component';
+import { SearchListComponent } from './feature/search-list/search-list.component';
+import { ToolbarComponent } from './feature/toolbar/toolbar.component';
+import { PreviewComponent } from './feature/preview/preview.component';
+import { CustomizeComponent } from './feature/customize/customize.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    SearchListComponent,
+    ToolbarComponent,
+    PreviewComponent,
+    CustomizeComponent
   ],
   imports: [
     BrowserModule
